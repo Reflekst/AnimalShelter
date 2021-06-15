1233f280a4e59343f26d6e25370cb8969c824566
Dariusz <49146540+Reflekst@users.noreply.github.com>
2021-06-15 04:22:52 +0200

    Numbers
    Script that checks divisibility by 3 and 5

--- Assets/Scripts/ObjectSelector.cs
+++ Assets/Scripts/ObjectSelector.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ObjectSelector : MonoBehaviour
 {
     public GameObject agentTemplate;
-    public Text nameText, hpText;
+    public Text nameText, hpText, counterText;
     private string targetName;
     private int targetHp;
     void Update()
@@ -20,29 +20,49 @@ public class ObjectSelector : MonoBehaviour
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 100)) ;
+        if (Physics.Raycast(ray, out hit, 100.0f))
         {
             if (hit.collider.tag == "AgentTag")
             {
                 AgentController target = hit.transform.GetComponent<AgentController>();
-                targetName = target.name;
-                targetHp = target.clone.CheckLifeValue;
-                ShowUi();
+                if (target != null)
+                {
+                    targetName = target.name;
+                    targetHp = target.clone.CheckLifeValue;
+                    ShowAgentUi();
+                }
             }
             else
             {
-                HideUi();
+                HideAgentUi();
             }
         }
     }
-    private void ShowUi()
+    private void ShowAgentUi()
     {
         agentTemplate.gameObject.SetActive(true);
         nameText.text = targetName;
         hpText.text = targetHp.ToString();
     }
-    private void HideUi()
+    private void HideAgentUi()
     {
         agentTemplate.gameObject.SetActive(false);
+    }
+    public void DisplayDividers()
+    {
+        string tmp = "";
+        for (int i = 1; i <= 100 ; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                tmp += "MarkoPolo";
+            else if (i % 3 == 0)
+               tmp += "Marko";
+            else if (i % 5 == 0)
+                tmp += "Polo";
+            else
+                tmp += i.ToString();
+            tmp += " ";
+        }
+        counterText.text = tmp;
     }
 }
